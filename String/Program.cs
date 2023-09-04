e4a9d95e826f7808a86e499c1011a05968111bde
ITaly177 Milan <patrokl2010@yandex.ru>
2023-09-04 10:46:58 +0300

    Add project files.

---
+++ String/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class StringDemo
+{
+    static void Main()
+    {
+        char[] symbs = { 'Я', 'з', 'ы', 'к', ',', 'C', '#' };
+        Console.WriteLine("symvolnii massiv:");
+            Console.Write(symbs);
+        String A = new String(symbs);
+        Console.WriteLine("A: \"{0}\"", A);
+        String B = new String(symbs, 1, 5);
+        Console.WriteLine("B: \"{0}\"", B);
+        String C = new String('ы', 7);
+        Console.WriteLine("C:\"{0}\"", C);
+        A = getText('A', 'H');
+        Console.WriteLine("A:  \"{0}\"", A);
+        B = getText('H', 'A');
+        Console.WriteLine("B:  \"{0}\"", B);
+        C = getText(symbs);
+        Console.WriteLine("C: \"{0}\"", C);
+    }
+    static String getText(char[] symbs)
+    {
+        String txt = "";
+        for(int k = 0; k < symbs.Length; k++)
+        {
+            txt += symbs[k];
+        }
+        return txt;
+    }
+    static String getText(char a,char b)
+    {
+        String txt = "";
+        char s = a;
+        while (s <= b)
+        {
+            txt += s;
+        }
+        return txt;
+    }
+}
